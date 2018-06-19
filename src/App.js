b8ac8ba5bc98317291de05bd443e3ea1ff5d8b0a
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-06-19 16:19:40 -0400

    add first components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Graph from './components/Graph.js';
 import './App.css';
 
 const API_KEY = '9GR1NJ63OZUJXWYA';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div>

31be775a93b7a32e69c763257cd3f4bedcc4df03
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-06-19 03:58:23 -0400

    about to delete registerservice

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const API_KEY = '9GR1NJ63OZUJXWYA';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=MSFT&apikey=demo
       </div>
     );
   }

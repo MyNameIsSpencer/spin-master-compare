4a351660b47b99662358e25d518c08028e8b2ecc
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-06-19 19:42:20 -0400

    update index.js src to include middleware, but will now change gears to youtube tutorial

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
-// import reducers from './reducers';
+import reducers from './reducers';
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
